bd3d7f0d1fd0224a99d0485c8f18ad3949fe1f44
Erk Sampat <erksampat@169-231-112-107.wireless.ucsb.edu>
2023-05-10 15:19:18 -0700

    formatting improvements; close file before quitting

--- eApprox.c
+++ eApprox.c
@@ -9,7 +9,7 @@ double quart_circ[CIRC_PTS - 2][2];
 double slopes[CIRC_PTS - 2];
 double e_approx[TOT_PTS][2];
 
-void quartCircAround(double *center) {
+void quartCircAround(double* center) {
     double init_angle = M_PI_2 / CIRC_PTS;
     double angle = init_angle;
     for (int i = 0; i < CIRC_PTS - 2; i++) {
@@ -19,13 +19,13 @@ void quartCircAround(double *center) {
     }
 }
 
-void getSlopes(double *center) {
+void getSlopes(double* center) {
     for (int i = 0; i < CIRC_PTS - 2; i++) {
         slopes[i] = (quart_circ[i][1] - center[1]) / (quart_circ[i][0] - center[0]);
     }
 }
 
-double *choosePt(void) {
+double* choosePt(void) {
     double difference = fabs(slopes[0] - quart_circ[0][1]);
     int index = 0;
     for (int i = 1; i < CIRC_PTS - 2; i++) {
@@ -41,8 +41,8 @@ double *choosePt(void) {
 int main (void) {
     e_approx[0][0] = 0;
     e_approx[0][1] = 1;
-    double *point;
-    FILE *fp;
+    double* point;
+    FILE* fp;
     fp = fopen("points.txt", "w+");
     // x, y, e^x, y - e^x, y^(1/x), y^(1/x) - e
     fprintf(fp, "%f %f %f %f %f %f\n", 0.0, 1.0, 1.0, 0.0, M_E, 0.0);
@@ -53,7 +53,10 @@ int main (void) {
         e_approx[i][0] = *point++;
         e_approx[i][1] = *point;
         // x, y, e^x, y - e^x, y^(1/x), y^(1/x) - e
-        fprintf(fp, "%f %f %f %f %f %f\n", e_approx[i][0], e_approx[i][1], exp(e_approx[i][0]), e_approx[i][1] - exp(e_approx[i][0]), pow(e_approx[i][1], 1 / e_approx[i][0]), pow(e_approx[i][1], 1 / e_approx[i][0]) - M_E);
+        fprintf(fp, "%f %f %f %f %f %f\n", e_approx[i][0], e_approx[i][1], exp(e_approx[i][0]), e_approx[i][1] - exp(e_approx[i][0]), 
+        pow(e_approx[i][1], 1 / e_approx[i][0]), pow(e_approx[i][1], 1 / e_approx[i][0]) - M_E);
     }
+
+    fclose(fp);
     return 0;
 }
