255e5c799bac43e7811d165d0664678038a7c9b3
Erk Sampat <erksampat@Erks-MacBook-Pro.local>
2023-09-08 18:20:47 -0700

    major restructuring

--- eApprox.c
+++ eApprox.c
@@ -1,62 +1,117 @@
 #include <stdio.h>
 #include <math.h>
 
-#define CIRC_PTS 100000
-#define RAD 0.01
-#define TOT_PTS 1000
+struct point {
+    double x;
+    double y;
+};
 
-double quart_circ[CIRC_PTS - 2][2];
-double slopes[CIRC_PTS - 2];
-double e_approx[TOT_PTS][2];
+void init(int* CIRC_PTS, int* TOT_PTS, double* RAD);
+void quartCircAround(struct point quart_circ[], struct point* center, int* CIRC_PTS, double* RAD);
+void getSlopes(double slopes[], struct point quart_circ[], struct point* center, int* CIRC_PTS);
+struct point choosePoint(double slopes[], struct point quart_circ[], int* CIRC_PTS);
 
-void quartCircAround(double* center) {
-    double init_angle = M_PI_2 / CIRC_PTS;
+int main(void) {
+    int CIRC_PTS = 100000;
+    int TOT_PTS = 1000;
+    double RAD = 0.01;
+
+    init(&CIRC_PTS, &TOT_PTS, &RAD);
+
+    struct point quart_circ[CIRC_PTS - 2];
+    double slopes[CIRC_PTS - 2];
+    struct point e_approx[TOT_PTS];
+
+    e_approx[0].x = 0;
+    e_approx[0].y = 1;
+
+    FILE* fp;
+    fp = fopen("points.txt", "w+");
+    // x, y, e^x, y - e^x, y^(1/x), y^(1/x) - e
+    fprintf(fp, "%f %f %f %f %f %f\n", 0.0, 1.0, 1.0, 0.0, M_E, 0.0);
+
+    for (int i = 1; i < TOT_PTS; i++) {
+        quartCircAround(quart_circ, &e_approx[i - 1], &CIRC_PTS, &RAD);
+        getSlopes(slopes, quart_circ, &e_approx[i - 1], &CIRC_PTS);
+        e_approx[i] = choosePoint(slopes, quart_circ, &CIRC_PTS);
+        // x, y, e^x, y - e^x, y^(1/x), y^(1/x) - e
+        fprintf(fp, "%f %f %f %f %f %f\n", e_approx[i].x, e_approx[i].y, exp(e_approx[i].x), e_approx[i].y - exp(e_approx[i].x), pow(e_approx[i].y, 1 / e_approx[i].x), pow(e_approx[i].y, 1 / e_approx[i].x) - M_E);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
+
+void init(int* CIRC_PTS, int* TOT_PTS, double* RAD) {
+    printf("\n---eApprox: e and e^x approximator by Erk Sampat---\n\n");
+    printf("Enter the following parameters. Defaults will be used in the case of \"0\" or invalid entries.\n\n");
+
+    printf("Enter a positive integer for the number of circle points (100,000 default): ");
+    int entry = 0;
+    if (!scanf("%d", &entry)) {
+        fflush(stdin);
+    }
+    if (entry > 0) {
+        *CIRC_PTS = entry;
+        printf("Number of circle points: %d\n\n", *CIRC_PTS);
+    }
+    else {
+        printf("Using default of %d.\n\n", *CIRC_PTS);
+    }
+
+    printf("Enter a positive integer for the total number of points (1,000 default): ");
+    entry = 0;
+    if (!scanf("%d", &entry)) {
+        fflush(stdin);
+    }
+    if (entry > 0) {
+        *TOT_PTS = entry;
+        printf("Number of total points: %d\n\n", *TOT_PTS);
+    }
+    else {
+        printf("Using default of %d.\n\n", *TOT_PTS);
+    }
+
+    printf("Enter a positive decimal for the circle radius (0.01 default): ");
+    double entry_d = 0.0;
+    if (!scanf("%lf", &entry_d)) {
+        fflush(stdin);
+    }
+    if (entry_d > 0.0) {
+        *RAD = entry_d;
+        printf("Radius: %lf\n\n", *RAD);
+    }
+    else {
+        printf("Using default of %lf.\n\n", *RAD);
+    }
+}
+
+void quartCircAround(struct point quart_circ[], struct point* center, int* CIRC_PTS, double* RAD) {
+    double init_angle = M_PI_2 / *CIRC_PTS;
     double angle = init_angle;
-    for (int i = 0; i < CIRC_PTS - 2; i++) {
-        quart_circ[i][0] = center[0] + RAD * cos(angle);
-        quart_circ[i][1] = center[1] + RAD * sin(angle);
+    for (int i = 0; i < *CIRC_PTS - 2; i++) {
+        quart_circ[i].x = center->x + *RAD * cos(angle);
+        quart_circ[i].y = center->y + *RAD * sin(angle);
         angle += init_angle;
     }
 }
 
-void getSlopes(double* center) {
-    for (int i = 0; i < CIRC_PTS - 2; i++) {
-        slopes[i] = (quart_circ[i][1] - center[1]) / (quart_circ[i][0] - center[0]);
+void getSlopes(double slopes[], struct point quart_circ[], struct point* center, int* CIRC_PTS) {
+    for (int i = 0; i < *CIRC_PTS - 2; i++) {
+        slopes[i] = (quart_circ[i].y - center->y) / (quart_circ[i].x - center->x);
     }
 }
 
-double* choosePt(void) {
-    double difference = fabs(slopes[0] - quart_circ[0][1]);
+struct point choosePoint(double slopes[], struct point quart_circ[], int* CIRC_PTS) {
+    double difference = fabs(slopes[0] - quart_circ[0].y);
     int index = 0;
-    for (int i = 1; i < CIRC_PTS - 2; i++) {
-        double a = fabs(slopes[i] - quart_circ[i][1]);
+    for (int i = 1; i < *CIRC_PTS - 2; i++) {
+        double a = fabs(slopes[i] - quart_circ[i].y);
         if (a < difference) {
             difference = a;
             index = i;
         }
     }
     return quart_circ[index];
-}
-
-int main (void) {
-    e_approx[0][0] = 0;
-    e_approx[0][1] = 1;
-    double* point;
-    FILE* fp;
-    fp = fopen("points.txt", "w+");
-    // x, y, e^x, y - e^x, y^(1/x), y^(1/x) - e
-    fprintf(fp, "%f %f %f %f %f %f\n", 0.0, 1.0, 1.0, 0.0, M_E, 0.0);
-    for (int i = 1; i < TOT_PTS; i++) {
-        quartCircAround(e_approx[i - 1]);
-        getSlopes(e_approx[i - 1]);
-        point = choosePt();
-        e_approx[i][0] = *point++;
-        e_approx[i][1] = *point;
-        // x, y, e^x, y - e^x, y^(1/x), y^(1/x) - e
-        fprintf(fp, "%f %f %f %f %f %f\n", e_approx[i][0], e_approx[i][1], exp(e_approx[i][0]), e_approx[i][1] - exp(e_approx[i][0]), 
-        pow(e_approx[i][1], 1 / e_approx[i][0]), pow(e_approx[i][1], 1 / e_approx[i][0]) - M_E);
-    }
-
-    fclose(fp);
-    return 0;
 }
